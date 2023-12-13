a0f77ee9e1bb4a09433e60fc09caeb5f21be9a9e
Moritz Schaefer <mail@moritzs.de>
2023-12-13 09:14:20 +0100

    enable full-tabulasapiens validation

--- src/single_cellm/validation/__init__.py
+++ src/single_cellm/validation/__init__.py
@@ -12,9 +12,11 @@ def initialize_validation_functions(
         "zero_shot_cancer_gene_essentiality": EvaluateCancerGeneEssentiality(
             batch_size, transcriptome_model_type, text_model_type
         ),
-        # "zero_shot_single_cell_annotations": SingleCellZeroshotValidationScoreCalculator(
-        #     batch_size=batch_size
-        # ),  # commented due to slow speed
+        "zero_shot_single_cell_annotations": SingleCellZeroshotValidationScoreCalculator(
+            batch_size=batch_size,
+            transcriptome_tokenizer_type=transcriptome_model_type,
+            tokenizer_name=text_model_type,
+        ),
         "zero_shot_single_cell_annotations_well_studied_celltypes": SingleCellZeroshotValidationScoreCalculator(
             celltypes=TOP20_LUNG_LIVER_BLOOD_CELLTYPES,
             batch_size=batch_size,
