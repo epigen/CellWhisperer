c3556ad835beef879a934e4ecd2af17ac01067c8
Moritz <mschaefer@cemm.at>
2024-02-26 19:22:54 +0100

    MedUni hosting script (#306)

---
+++ hosting/muwhpc/README.md
@@ -0,0 +1,2 @@
+This is just a temporal "one-time" solution, since I am unable to restart my home server these days.
+Also the `main.sh` script is not debugged
---
+++ hosting/muwhpc/main.sh
@@ -0,0 +1,10 @@
+cd cellwhisperer
+python src/model_server/main.py &
+sleep 20
+cellxgene launch --debug -v -p 5005  --host 0.0.0.0 --max-category-items 500 --backed ~/cellwhisperer/results/tabula_sapiens/03jujd8s/cellxgene.h5ad  "http://localhost:8910/api" &
+cellxgene launch --debug -v -p 5006  --host 0.0.0.0 --max-category-items 500 --backed ~/cellwhisperer/results/archs4_metasra/03jujd8s/cellxgene.h5ad  "http://localhost:8910/api" &
+source activate nginx
+nginx -c /msc/home/mschae83/cellwhisperer/hosting/muwhpc/nginx.conf
+source activate ngrok
+ngrok http 5100 --basic-auth "review:iclr2024-mlgenx" --domain upright-communal-panda.ngrok-free.app --log=stdout > ngrok.log
+
